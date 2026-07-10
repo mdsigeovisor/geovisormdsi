05c3841b7d737d0aa30d9d44b1503a104fb2c0ba
Jorge Chafloque <jchafloque.gis@gmail.com>
2026-07-10 02:51:14 -0500

    10-07-2026

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,10 @@ export const environment = {
   geoserver: {
     // Servidor MSI proporcionado
     serverUrl: 'http://192.168.40.58:8081/geoserver',
+    serverUrlViejo: 'http://192.168.41.147:8080/geoserver',
     serverImpresionLocal: 'http://172.16.16.67:8080/geoserver',
     // Workspace compartido para todas las capas y consultas OGC
+
     workspace: 'SIDES_GIS',
     get workspacePrefix(): string {
       return `${this.workspace}:`;
@@ -27,4 +29,24 @@ export const environment = {
     transparent: true
   },
 
+  // --- CONFIGURACIÓN DE GEOSERVER DE PRUEBA (ORTOFOTOGRAFÍAS) ---
+  geoserverOrtofotografias: {
+    serverUrl: 'https://192.168.41.147:8080/geoserver',
+    workspace: 'ide_ortofotografias',
+    get workspacePrefix(): string {
+      return `${this.workspace}:`;
+    },
+    get baseUrl(): string {
+      return `${this.serverUrl}/${this.workspace}`;
+    },
+    get wmsUrl(): string {
+      return `${this.baseUrl}/wms`;
+    },
+    get owsUrl(): string {
+      return `${this.baseUrl}/ows`;
+    },
+    formatWMS: 'image/png',
+    srs: 'EPSG:32718',
+    transparent: true
+  }
 };

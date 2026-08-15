4749248fa78c1d0c5b99eff17474b4ec452af7d6
Jorge Chafloque <jchafloque.gis@gmail.com>
2026-08-15 11:29:16 -0500

    Termino y condiciones

--- src/app/components/auth/components/bienvenido/bienvenido.ts
+++ src/app/components/auth/components/bienvenido/bienvenido.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bienvenido',
   standalone: true,
-  imports: [
-    RouterLink,
-    FormsModule,
-  ],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './bienvenido.html',
   styleUrl: './bienvenido.css',
 })
 export class Bienvenido {
   termsAccepted = false;
+  showTermsModal = false;
+  showPrivacyModal = false;
   currentYear: number = new Date().getFullYear();
 
   onLogin(): void {
